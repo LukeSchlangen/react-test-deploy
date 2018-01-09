22b17b8118f89aa5673afddec3c361dc551b3a20
Luke Schlangen <lukeschlangen@Lukes-MacBook-Pro.local>
2018-01-09 14:21:05 -0600

    add routing

---
+++ src/components/App/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Hello from '../HelloComponent/HelloComponent';
+import About from '../AboutComponent/AboutComponent';
+import Books from '../BooksComponent/BooksComponent';
+
+// import route Components here
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  // Switch,
+  // Redirect
+} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <ul>
+              <ul>
+                <li><Link to="/hello">Hello</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/books">Books</Link></li>
+              </ul>
+            </ul>
+            <hr />
+            <Route path="/hello" component={Hello} />
+            <Route path="/about" component={About} />
+            <Route path="/books" component={Books} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+export default App;
